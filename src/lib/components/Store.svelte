5ca6ee884834d017fc6086e79053ba14fc19db3c
Enderpicture <enderpicture@gmail.com>
2022-07-15 22:03:47 -0700

    template

---
+++ src/lib/components/Store.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { test } from '$lib/store';
+</script>
+
+<p>this is a store {$test}</p>
+
+<button on:click={() => {$test = $test + "hello"}}>dasdfsad</button>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,11 @@
+<script lang="ts">
+	import Store from '$lib/components/Store.svelte';
+	import { test } from '$lib/store';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+<p>test text {$test}</p>
 
-<p>test text</p>
+<Store />
