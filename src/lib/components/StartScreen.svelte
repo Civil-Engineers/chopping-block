fd7b1b7b2155bc78acc7c2e69d4140df4f1f6308
Enderpicture <enderpicture@gmail.com>
2022-07-17 06:48:07 -0700

    fix start button error

--- src/lib/components/StartScreen.svelte
+++ src/lib/components/StartScreen.svelte
@@ -1,19 +1,22 @@
+<script lang="ts">
+	import { EGlobalStates, globalGameState } from '$lib/store';
+</script>
 
 <div class="con">
-  <h1>Chopping Block</h1>
-  <button on:click={() => window.location.reload()}>Start</button>
+	<h1>Chopping Block</h1>
+	<button on:click={() => ($globalGameState = EGlobalStates.BATTLE)}>Start</button>
 </div>
 
 <style lang="scss">
-  * {
-    box-sizing: border-box;
-  }
-  .con {
-    width: 100%;
-    height: 100%;
-    background-color: cornsilk;
-    padding: 1rem;
-  }
+	* {
+		box-sizing: border-box;
+	}
+	.con {
+		width: 100%;
+		height: 100%;
+		background-color: cornsilk;
+		padding: 1rem;
+	}
 	h1 {
 		margin-top: 100px;
 		text-align: center;
@@ -28,10 +31,10 @@
 		border: none;
 		border-radius: 30px;
 		padding: 10px 30px;
-    transition: .5s ease;
-    &:hover {
-      cursor: pointer;
-      transform: translateY(-20px) rotate(-5deg)
-    }
+		transition: 0.5s ease;
+		&:hover {
+			cursor: pointer;
+			transform: translateY(-20px) rotate(-5deg);
+		}
 	}
 </style>
