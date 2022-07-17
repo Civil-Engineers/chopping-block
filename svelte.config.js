2703e1d26f4b16fefef716bd75d3473ff9569c76
Enderpicture <enderpicture@gmail.com>
2022-07-16 22:31:37 -0700

    do statically render

--- svelte.config.js
+++ svelte.config.js
@@ -21,7 +21,12 @@ const config = {
 			assets: 'build',
 			fallback: null,
 			precompress: false
-		})
+		}),
+
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	}
 };
 
