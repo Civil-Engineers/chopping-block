a3a9ec5a69625fbad39d689e2df703858d6d823c
Enderpicture <enderpicture@gmail.com>
2022-07-16 23:32:49 -0700

    added probability ability generator

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,7 +1,19 @@
+import { allAbilities } from './store';
+
 export function sleep(ms: number) {
 	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 export function rollDice(facesNum: number) {
 	return Math.floor(Math.random() * facesNum);
+}
+
+export function getRandomAbility() {
+	let probabilityArray = Object.keys(allAbilities).reduce((arr, key) => {
+		arr.push(...new Array(allAbilities[key].rarity).fill(key));
+		return arr;
+	}, [] as number[]);
+
+  const chosenNumber = Math.floor(Math.random()*probabilityArray.length);
+  return allAbilities[probabilityArray[chosenNumber]];
 }
