ded04b272d23b9491b4b37dcc6d21c7ac7eb6a69
Alvin <ho.alvin857@gmail.com>
2022-07-17 11:42:00 -0700

    Update Instructions.svelte

--- src/lib/components/Instructions.svelte
+++ src/lib/components/Instructions.svelte
@@ -3,7 +3,21 @@
 </script>
 
 <div class="con">
-	<h1>Instructions</h1>
+	<h1 class="titl">Instructions</h1>
+	<p class="inst">How to play! <br><br>
+		You have two 6 sided dice with each face representing an ability. <br><br>
+		These abilities are randomized during the fight and used against the enemy!<br><br> 
+		These are the abilities:<br>
+		Fist --> Deals damage<br>
+		Shield -->Block incoming damage<br>
+		2x --> Multiplies Damage <br>
+		Heart --> Restores HP<br>
+		Gold --> Get money <br>
+		Axe --> Deals Damage to ALL enemies*<br><br>
+		
+		These abilities can be switched out and bought in the shop after a successful win in a battle.
+		<br><br>
+		Fight all the enemies to get to the boss!</p>
 	<button
 		on:click={() => {
 			$globalGameState = EGlobalStates.BATTLE;
@@ -23,9 +37,14 @@
 		padding: 1rem;
 	}
 	h1 {
-		margin-top: 100px;
+		margin-top: 20px;
 		text-align: center;
-		font-size: 200px;
+		font-size: 80px;
+		margin-bottom: 30px;
+	}
+	p {
+		text-align: center;
+		font-size: 15px;
 		margin-bottom: 30px;
 	}
 	button {
