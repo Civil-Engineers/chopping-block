7c3416be2e45efd0a3f415fdd696b4b8e2144827
Enderpicture <enderpicture@gmail.com>
2022-07-17 06:45:46 -0700

    adding lose and win screens

--- src/lib/components/StartScreen.svelte
+++ src/lib/components/StartScreen.svelte
@@ -1,6 +1,37 @@
-<script lang="ts">
-	import { globalGameState, EGlobalStates } from '$lib/store';
-</script>
 
-<h1>start screen</h1>
-<button on:click={() => ($globalGameState = EGlobalStates.BATTLE)}>play</button>
+<div class="con">
+  <h1>Chopping Block</h1>
+  <button on:click={() => window.location.reload()}>Start</button>
+</div>
+
+<style lang="scss">
+  * {
+    box-sizing: border-box;
+  }
+  .con {
+    width: 100%;
+    height: 100%;
+    background-color: cornsilk;
+    padding: 1rem;
+  }
+	h1 {
+		margin-top: 100px;
+		text-align: center;
+		font-size: 200px;
+		margin-bottom: 30px;
+	}
+	button {
+		display: block;
+		margin-right: auto;
+		margin-left: auto;
+		font-size: 100px;
+		border: none;
+		border-radius: 30px;
+		padding: 10px 30px;
+    transition: .5s ease;
+    &:hover {
+      cursor: pointer;
+      transform: translateY(-20px) rotate(-5deg)
+    }
+	}
+</style>
